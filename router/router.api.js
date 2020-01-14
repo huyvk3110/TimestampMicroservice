40892cdc5c7ba0a1296cfa3d90c0a421b13814ff
Van Huy <huyvk3110@gmail.com>
2020-01-14 16:49:42 +0700

    Check time number

--- router/router.api.js
+++ router/router.api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/timestamp/:date_string*?', function (req, res) {
     const { date_string } = req.params;
-    const date = date_string ? !date_string.match(/\W/) ? new Date(parseInt(date_string)) : new Date(date_string) : new Date();
+    const date = date_string ? /\d{5,}/.test(date_string) ? new Date(parseInt(date_string)) : new Date(date_string) : new Date(new Date().getTime());
     if (isNaN(date.getTime())) res.json({ error: date.toString() })
     else res.json({ unix: date.getTime(), utc: date.toUTCString() })
 })
